1d5ac0c761457c8c33fe06cb0a6f7e084c0c2bc6
ritesh-khandekar <riteshkhandekar4003@gmail.com>
2022-08-29 00:37:50 +0530

    frontend modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
 import React from 'react';
-
-import Table from "./components/admin-components/Table";
+import { Route, Routes } from "react-router-dom";
+import Table from "./components/Table";
+import NavbarLight from "./components/navbar/NavbarAdminLight";
+import ComplaintForm from "./components/Complaintform";
 
 // import ComplaintForm from "./components/Complaintform";
 
 function App() {
   return (
     <>
-    
- <Table/>
+    <NavbarLight />
+    <Routes>
+      <Route path='/admin' element={<Table />} />
+      <Route path='/complaint' element={<ComplaintForm/>} />
+
+    </Routes>
     </>
   );
 }
--- frontend/src/components/Complaintform.js
+++ frontend/src/components/Complaintform.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TextArea from './TextArea'
+
 function ComplaintForm() {
   return (
     <>
