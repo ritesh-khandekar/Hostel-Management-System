e70fcf8d4ef0ab33b4b0306d1eca953ae73025e2
DavidJennicson <85760802+DavidJennicson@users.noreply.github.com>
2022-08-29 10:37:27 +0530

    table frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 
 import Table from "./components/Table";
 import TableOptions from "./components/TableOptions";
-
+import logo from './components/crce-logo.svg'
 // import ComplaintForm from "./components/Complaintform";
 
 function App() {
   return (
     <>
+    <div className="icon">
+  <img id="clg-logo" src={logo} alt="" />
+</div>
     <TableOptions total="4" pending="2" solved="2"/>
+
  <Table/>
     </>
   );
